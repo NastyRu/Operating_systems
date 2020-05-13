6c35e7870d14ee983766a58ca0f5cfc21f1fc417
NastyRu <nasty.sidenko@mail.ru>
2020-05-13 21:36:08 +0300

    Update lab

--- semestr2/lab3/task1/md.c
+++ semestr2/lab3/task1/md.c
@@ -9,14 +9,25 @@ MODULE_DESCRIPTION("Lab3");
 // Инициализация модуля
 static int __init my_module_init(void)
 {
-   printk(KERN_INFO "Start!\n");
+   printk(KERN_INFO "MODULE1: loaded!\n");
+
+   struct task_struct *task = &init_task;
+   do
+   {
+     printk(KERN_INFO "MODULE1: process: %s - %d, parent: %s - %d\n",
+     task->comm, task->pid, task->parent->comm, task->parent->pid);
+   }
+   while ((task = next_task(task)) != &init_task);
+
+   printk(KERN_INFO "MODULE1: current: %s — %d, parent: %s — %d\n",
+   current->comm, current->pid, current->parent->comm, current->parent->pid);
    return 0;
 }
 
 // Удаление модуля
 static void __exit my_module_exit(void)
 {
-   printk(KERN_INFO "End!\n");
+   printk(KERN_INFO "MODULE1: unloaded\n");
 }
 
 module_init(my_module_init);
